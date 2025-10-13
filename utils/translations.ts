b1537404d7ec9945b0e0f141023d17bb01dc6703
learneverythingandtryit <learneverythingandtryit@gmail.com>
2025-10-14 03:07:50 +0530

    Delete utils directory

--- utils/translations.ts
+++
@@ -1,9 +0,0 @@
-// This file has been removed as the translation system is no longer used
-// All components now use hardcoded English text
-
-export type LanguageCode = 'english';
-export type Translation = {};
-
-export function getTranslation(language: LanguageCode): Translation {
-  return {};
-}
