f5cdb7d52a394d5aa825c0dd922817cb8178866f
learneverythingandtryit <learneverythingandtryit@gmail.com>
2025-10-14 03:20:54 +0530

    Delete src directory

--- src/components/LanguageSelector.tsx
+++
@@ -1,6 +0,0 @@
-// This component has been removed as the translation system is no longer used
-// All components now use hardcoded English text
-
-export function LanguageSelector() {
-  return null;
-}
