c1bccf7f8bba92ff0645b13bd4880d92e94e923b
learneverythingandtryit <learneverythingandtryit@gmail.com>
2025-10-14 01:14:58 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/', // 👈 Ensures correct routing and asset paths for Render
   build: {
     outDir: 'dist',
